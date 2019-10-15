4d8e7a20044f0c8ae746998dd41849a46e12e8dd
Filipe Ramos <ik.ben.filipe@gmail.com>
2019-10-15 12:29:27 -0300

    Definição de AFD em Coq + Delta total relatório

--- dfa.v
+++ dfa.v
@@ -1,52 +1,39 @@
-(* We can define word as list of events ;-) *)
-Inductive word {E:Type} : Type :=
-  | empty
-  | headw (e : E) (w : @word E).
+Record dfa (Q E : Type) := {
+  delta : Q -> E -> Q;
+  initial_state : Q;
+  is_final : Q -> bool
+}.
 
-Notation "e :: w" := (headw e w)
-                     (at level 60, right associativity).
-Notation "[ ]" := empty.
-Notation "[ x ; .. ; y ]" := (headw x .. (headw y empty) ..).
+(* Example *)
 
+Inductive states1 : Type :=
+  | q0 (* initial state *)
+  | q1
+  | q2 (* final state *)
+  | q3. (* ? *)
 
+Inductive events1 : Type :=
+  | a
+  | b.
 
-Inductive DFA : Type :=
-  | dfa (Q E : Type) (delta : Q->E->Q) (q0 : Q) (Qm : list Q) (n : nat).
-
-Notation "( Q , E , delta , q0 , Qm , n )" := (dfa Q E delta q0 Qm n).
-
-
-
-Fixpoint extended_fun {Q E : Type} (f : Q->(@word E)->Q) (q : Q) (w : word)
- : Q :=
-  match w with
-  | [] => q
-  | e::w' => extended_fun f (f q e) w'
+Definition delta1 (q:states1) (e:events1) : states1 :=
+  match q, e with
+  | q0, a => q1
+  | q1, b => q2
+  | q2, a => q1
+  | _, _ => q3
   end.
 
-
-
-Definition language {E : Type} (G : DFA) : Type :=
-  match G with
-  | (Q,E,delta,q0,Qm,n) =>
+Definition is_final1 (q:states1) : bool :=
+  match q with
+  | q2 => true
+  | _ => false
   end.
 
+Definition dfa1 :=
+  {| delta := delta1; initial_state := q0; is_final := is_final1 |}.
+
+Check dfa1.
 
 
 
-Inductive events {E : Type} : Type :=
-  | add
-  | rem
-  | other (e : E).
-
-
-
-Fixpoint buffer_count {E : Type} (w : @word (@events E)) : nat :=
-  match w with
-  | [] => 0
-  | add::w' => buffer_count w' + 1
-  | rem::w' => buffer_count w' - 1
-  | _::w' => buffer_count w'
-  end.
-
-Compute buffer_count [add;add;rem;rem;rem]. (* shall be -1 *)
