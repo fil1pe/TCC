51aead7fa64a419deb115fec101208eed2d2e795
Filipe Ramos <ik.ben.filipe@gmail.com>
2019-10-16 09:31:05 -0300

    Mudança nos nomes

---
+++ DFA.v
@@ -0,0 +1,102 @@
+Require Import Coq.Lists.List.
+Import ListNotations.
+
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
+
+Inductive states {Q : Type} : Type :=
+  | sink_state
+  | state (q : Q).
+
+Record dfa (Q E : Type) := {
+  delta: Q -> E -> @states Q;
+  initial_state: Q;
+  is_final: Q -> bool
+}.
+
+(* Extended transition function: *)
+
+Fixpoint extended_delta {Q E : Type} (g : dfa Q E) (q : @states Q) (w : list E) : @states Q :=
+  match q with
+  | sink_state => sink_state
+  | state q' => match w with
+                | [] => state q'
+                | e::w' => extended_delta g (g.(delta) q' e) w'
+                end
+  end.
+
+Theorem dist_extended_delta:
+  forall (Q E : Type) (g : dfa Q E) w w' q,
+    extended_delta g q (w ++ w') =
+    extended_delta g (extended_delta g q w) w'.
+Proof.
+  intros.
+  generalize dependent q.
+  generalize dependent w'.
+  induction w.
+  - intros. simpl. destruct q. reflexivity. reflexivity.
+  - intros. simpl. destruct q.
+    + destruct w'. reflexivity. reflexivity.
+    + rewrite IHw. reflexivity.
+Qed.
+
+(* Generated language: *)
+
+Definition in_language {Q E : Type} (g : dfa Q E) (w : list E) : Prop :=
+  ~ extended_delta g (state g.(initial_state)) w = sink_state.
+
+Notation " x ==> g " := (in_language g x) (at level 60). (* ? *)
+
+Theorem prefix_closed:
+  forall (Q E : Type) (g : dfa Q E) w w',
+    (w ++ w') ==> g -> w ==> g.
+Proof.
+  unfold in_language, not.
+  intros.
+  rewrite dist_extended_delta in H. rewrite H0 in H. apply H.
+  destruct w'. reflexivity. reflexivity.
+Qed.
+
+(*
+Example:
+
+Inductive states1 : Type :=
+  | q0 (* initial state *)
+  | q1
+  | q2 (* final state *).
+
+Inductive events1 : Type :=
+  | a
+  | b.
+
+Definition delta1 (q:states1) (e:events1) : @states states1 :=
+  match q, e with
+  | q0, a => state q1
+  | q1, b => state q2
+  | q2, a => state q1
+  | _, _ => sink_state
+  end.
+
+Definition is_final1 (q:states1) : bool :=
+  match q with
+  | q2 => true
+  | _ => false
+  end.
+
+Definition dfa1 :=
+  {| delta := delta1; initial_state := q0; is_final := is_final1 |}.
+
+Check dfa1.
+
+Compute extended_delta dfa1 (state q0) [a;b;a;b;a;b].
+
+Theorem dfa1_test1 : [a;b;a;b;a;b] ==> dfa1.
+Proof.
+  unfold in_language.
+  unfold not.
+  intros.
+  discriminate H.
+Qed.
+*)
+
--- QueeingSystems.v
+++ QueeingSystems.v
@@ -5,100 +5,7 @@ Set Implicit Arguments.
 Unset Strict Implicit.
 Unset Printing Implicit Defensive.
 
-Inductive states {Q : Type} : Type :=
-  | sink_state
-  | state (q : Q).
-
-Record dfa (Q E : Type) := {
-  delta : Q -> E -> @states Q;
-  initial_state : Q;
-  is_final : Q -> bool
-}.
-
-(* Extended transition function: *)
-
-Fixpoint extended_delta {Q E : Type} (g : dfa Q E) (q : @states Q) (w : list E) : @states Q :=
-  match q with
-  | sink_state => sink_state
-  | state q' => match w with
-                | [] => state q'
-                | e::w' => extended_delta g (g.(delta) q' e) w'
-                end
-  end.
-
-Theorem dist_extended_delta:
-  forall (Q E : Type) (g : dfa Q E) w w' q,
-    extended_delta g q (w ++ w') =
-    extended_delta g (extended_delta g q w) w'.
-Proof.
-  intros.
-  generalize dependent q.
-  generalize dependent w'.
-  induction w.
-  - intros. simpl. destruct q. reflexivity. reflexivity.
-  - intros. simpl. destruct q.
-    + destruct w'. reflexivity. reflexivity.
-    + rewrite IHw. reflexivity.
-Qed.
-
-(* Generated language: *)
-
-Definition in_language {Q E : Type} (g : dfa Q E) (w : list E) : Prop :=
-  ~ extended_delta g (state g.(initial_state)) w = sink_state.
-
-Notation " x ==> g " := (in_language g x) (at level 60). (* ? *)
-
-Theorem prefix_closed:
-  forall (Q E : Type) (g : dfa Q E) w w',
-    (w ++ w') ==> g -> w ==> g.
-Proof.
-  unfold in_language, not.
-  intros.
-  rewrite dist_extended_delta in H. rewrite H0 in H. apply H.
-  destruct w'. reflexivity. reflexivity.
-Qed.
-
-(*
-Example:
-
-Inductive states1 : Type :=
-  | q0 (* initial state *)
-  | q1
-  | q2 (* final state *).
-
-Inductive events1 : Type :=
-  | a
-  | b.
-
-Definition delta1 (q:states1) (e:events1) : @states states1 :=
-  match q, e with
-  | q0, a => state q1
-  | q1, b => state q2
-  | q2, a => state q1
-  | _, _ => sink_state
-  end.
-
-Definition is_final1 (q:states1) : bool :=
-  match q with
-  | q2 => true
-  | _ => false
-  end.
-
-Definition dfa1 :=
-  {| delta := delta1; initial_state := q0; is_final := is_final1 |}.
-
-Check dfa1.
-
-Compute extended_delta dfa1 (state q0) [a;b;a;b;a;b].
-
-Theorem dfa1_test1 : [a;b;a;b;a;b] ==> dfa1.
-Proof.
-  unfold in_language.
-  unfold not.
-  intros.
-  discriminate H.
-Qed.
-*)
+Require Import DFA.
 
 Require Export Arith_base.
 Require Import BinPos BinInt BinNat Pnat Nnat.
@@ -154,7 +61,7 @@ Proof.
     + rewrite IHw. reflexivity.
 Qed.
 
-Lemma sla3: forall (Q E : Type) (g : dfa Q E) (q : Q) (e : E),
+Lemma extended_delta__delta: forall (Q E : Type) (g : dfa Q E) (q : Q) (e : E),
   extended_delta g (state q) [e] = g.(delta) q e.
 Proof.
   intros.
@@ -164,7 +71,7 @@ Proof.
   reflexivity.
 Qed.
 
-Lemma th2:
+Lemma buffer_count_leq_f:
   forall (Q E : Type) (g : dfa Q (@events E)) (f : @states Q->Z),
     (f(state g.(initial_state)) = 0 /\
       forall (q : Q),
@@ -192,7 +99,7 @@ Proof.
       { rewrite dist_extended_delta. remember (extended_delta g (state (initial_state g)) w). destruct s.
         - apply prefix_closed in H0. unfold in_language in H0. unfold not in H0. symmetry in Heqs. apply H0 in Heqs.
           destruct Heqs.
-        - rewrite sla3. assert (f (delta g q add) >= f (state q) + 1).
+        - rewrite extended_delta__delta. assert (f (delta g q add) >= f (state q) + 1).
           { apply H1. }
           omega. }
       omega.
@@ -204,7 +111,7 @@ Proof.
       { rewrite dist_extended_delta. remember (extended_delta g (state (initial_state g)) w). destruct s.
         - apply prefix_closed in H0. unfold in_language in H0. unfold not in H0. symmetry in Heqs. apply H0 in Heqs.
           destruct Heqs.
-        - rewrite sla3. assert (f (delta g q rem) >= f (state q) - 1).
+        - rewrite extended_delta__delta. assert (f (delta g q rem) >= f (state q) - 1).
           { apply H2. }
           omega. }
       omega.
@@ -216,7 +123,7 @@ Proof.
       { rewrite dist_extended_delta. remember (extended_delta g (state (initial_state g)) w). destruct s.
         - apply prefix_closed in H0. unfold in_language in H0. unfold not in H0. symmetry in Heqs. apply H0 in Heqs.
           destruct Heqs.
-        - rewrite sla3. assert (f (delta g q (other e)) >= f (state q)).
+        - rewrite extended_delta__delta. assert (f (delta g q (other e)) >= f (state q)).
           { apply H3. }
           omega. }
       omega.
@@ -242,7 +149,7 @@ Proof.
     apply H.
   - destruct x.
     + assert (forall w, w ==> g -> buffer_count w <= f (extended_delta g (state g.(initial_state)) w)).
-      { apply th2. split.
+      { apply buffer_count_leq_f. split.
           apply H1. 
           intros. split.
           - apply H2.
@@ -263,4 +170,3 @@ Proof.
     + apply prefix_closed in H0. apply IHw in H0.
       rewrite buffer_other. apply H0.
 Qed.
-
