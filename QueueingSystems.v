23afd14f779d2e7d8c6ef8ec901adeb1af19651a
Filipe Ramos <ik.ben.filipe@gmail.com>
2019-11-06 16:21:18 -0300

    Escrevendo

--- QueueingSystems.v
+++ QueueingSystems.v
@@ -24,8 +24,8 @@ Fixpoint count_buffer {E : Type} (w : list (@event E)) : Z :=
   e::w' => count_event e + count_buffer w'
   end.
 
-Definition upper_bound {Q E : Type} (g : dfa Q (@event E)) n :=
-  forall w, g ==> w -> count_buffer w <= n.
+Definition upper_bound {Q E : Type} (g : dfa Q (@event E)) n0 n :=
+  forall w, g ==> w -> n0 + count_buffer w <= n.
 
 Lemma buffer_change: forall (E : Type) (w : list (@event E)) (e : @event E),
   count_buffer (w ++ [e]) = count_buffer w + count_event e.
@@ -37,15 +37,15 @@ Proof.
 Qed.
 
 Lemma buffer_count_leq_f:
-  forall (Q E : Type) (g : dfa Q (@event E)) (f : @state Q->Z),
-    (f(proper_state (initial_state g)) = 0 /\
+  forall (Q E : Type) (g : dfa Q (@event E)) (f : @state Q->Z) n0,
+    (f(proper_state (initial_state g)) = n0 /\
       forall (q : Q),
         f((transition g) q add) >= f(proper_state q) + 1 /\
         f((transition g) q rem) >= f(proper_state q) - 1 /\
         (forall (e : E), f((transition g) q (other e)) >= f(proper_state q))
-    ) -> (forall w, g ==> w -> count_buffer w <= f (extended_transition g (proper_state (initial_state g)) w)).
+    ) -> (forall w, g ==> w -> n0 + count_buffer w <= f (extended_transition g (proper_state (initial_state g)) w)).
 Proof.
-  intros Q E g f H0 w H2.
+  intros Q E g f n0 H0 w H2.
   destruct H0 as [H0 H1].
   induction w as [|e w IHw] using @rev_ind.
   - simpl. omega.
@@ -65,19 +65,19 @@ Proof.
 Qed.
 
 Theorem th1:
-  forall (Q E : Type) (g : dfa Q (@event E)) n,
-    (exists (f : @state Q->Z), f(proper_state (initial_state g)) = 0 /\
+  forall (Q E : Type) (g : dfa Q (@event E)) n0 n,
+    (exists (f : @state Q->Z), f(proper_state (initial_state g)) = n0 /\
       forall (q : Q),
         f(proper_state q) <= n /\
         f((transition g) q add) >= f(proper_state q) + 1 /\
         f((transition g) q rem) >= f(proper_state q) - 1 /\
         (forall (e : E), f((transition g) q (other e)) >= f(proper_state q))
-    ) -> upper_bound g n.
+    ) -> upper_bound g n0 n.
 Proof.
   unfold upper_bound.
-  intros Q E g n H0 w H2.
+  intros Q E g n0 n H0 w H2.
   destruct H0 as [f [H0 H1]].
-  assert (H3: count_buffer w <= f (extended_transition g (proper_state (initial_state g)) w)).
+  assert (H3: n0 + count_buffer w <= f (extended_transition g (proper_state (initial_state g)) w)).
   { apply buffer_count_leq_f.
       - split.
         + apply H0.
@@ -93,9 +93,9 @@ Proof.
 Qed.
 
 Theorem th2:
-  forall (Q E : Type) (g : dfa Q (@event E)) n,
-    upper_bound g n ->
-    (exists (f : @state Q->Z), f(proper_state (initial_state g)) = 0 /\
+  forall (Q E : Type) (g : dfa Q (@event E)) n0 n,
+    upper_bound g n0 n ->
+    (exists (f : @state Q->Z), f(proper_state (initial_state g)) = n0 /\
       forall (q : Q),
         f(proper_state q) <= n /\
         f((transition g) q add) >= f(proper_state q) + 1 /\
@@ -107,28 +107,16 @@ Proof.
 Admitted.
 
 Theorem th12:
-  forall (Q E : Type) (g : dfa Q (@event E)) n,
-    (exists (f : @state Q->Z), f(proper_state (initial_state g)) = 0 /\
+  forall (Q E : Type) (g : dfa Q (@event E)) n0 n,
+    (exists (f : @state Q->Z), f(proper_state (initial_state g)) = n0 /\
       forall (q : Q),
         f(proper_state q) <= n /\
         f((transition g) q add) >= f(proper_state q) + 1 /\
         f((transition g) q rem) >= f(proper_state q) - 1 /\
         (forall (e : E), f((transition g) q (other e)) >= f(proper_state q))
-    ) <-> upper_bound g n.
+    ) <-> upper_bound g n0 n.
 Proof.
   split.
   apply th1.
   apply th2.
-Qed.
-
-Theorem upper_bound_geq_0:
-  forall (Q E : Type) (g : dfa Q (@event E)) n,
-    upper_bound g n -> n >= 0.
-Proof.
-  intros Q E g n H.
-  apply th12 in H.
-  destruct H as [f H], H as [H H0].
-  assert (f (proper_state (initial_state g)) <= n).
-  { apply H0. }
-  omega.
 Qed.
