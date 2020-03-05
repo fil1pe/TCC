7c5c643b96fbccda05454071a17d13f6549356ee
Filipe Ramos <ik.ben.filipe@gmail.com>
2020-03-04 23:39:33 -0300

    Definição da tripla AFD

--- DFA.v
+++ DFA.v
@@ -2,6 +2,11 @@ Require Import Coq.Init.Nat Coq.Lists.List Omega.
 Require BinIntDef.
 Local Open Scope Z_scope.
 
+Definition state := nat.
+
+Definition states_num := 6%nat.
+(* Axiom states_num : nat. *)
+
 Inductive event := add | rem | oth.
 
 Definition count_event e :=
@@ -11,10 +16,7 @@ Definition count_event e :=
     oth =>  0
   end.
 
-Definition states_num := 6%nat.
-(* Axiom states_num : nat. *)
-
-Definition transition (q:nat) (e:event) :=
+Definition transition q e :=
   match q, e with
     0%nat, add => 5%nat |
     0%nat, rem => 1%nat |
@@ -26,3 +28,15 @@ Definition transition (q:nat) (e:event) :=
       _  ,  _  => 10%nat
   end.
 (* Axiom transition : nat->event->nat. *)
+
+Definition is_marked (q:nat) :=
+  match q with
+    _ => false
+  end.
+
+Definition DFA := (states_num, transition, is_marked).
+
+(*
+  The initial state of the DFA is the state 0.
+  The sink state is any state n where n >= states_num.
+*)
