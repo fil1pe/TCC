e2199813fdd066ae7ae70448ff9be2b960ee0593
Filipe Ramos <ik.ben.filipe@gmail.com>
2020-03-18 00:45:04 -0300

    Troquei a posição de marcação da lista solução para o começo, acho que facilitará a vida

--- QSUtils.v
+++ QSUtils.v
@@ -16,12 +16,6 @@ Definition optZ_ge (a b:option Z) :=
       _   ,   _    => false
   end.
 
-(* Fixpoint count_none (l:list (option Z)) : nat :=
-  match l with
-     []  => 0                                                        |
-    x::l => if optZ_eq x None then 1 + count_none l else count_none l
-  end. *)
-
 Fixpoint initial_solution (n:nat) : list (option Z) :=
   match n with
      O    => []                          |
@@ -35,24 +29,22 @@ Fixpoint update {A} (l:list (option A)) (n:nat) (a:A) :=
      l  ,  _  => l
   end.
 
-Fixpoint update_last {A} (o:A) (s:list (option A)) :=
-  match s with
-    x::[] => [Some o]           |
-    x::l  => x::update_last o l |
-      []  => []
-  end.
-
 Fixpoint foreach {A B} (l:list A) (f:A->B) (c:B->B->B) (b:B) :=
   match l with
      []  => b                        |
     x::l => c (f x) (foreach l f c b)
   end.
 
-Fixpoint all_but_last_le l n :=
+Fixpoint all_le l n :=
   match l with
-      x::[]   => true                             |
-    Some m::l => (m <=? n) && all_but_last_le l n |
-       _      => true
+    Some m::l => (m <=? n) && all_le l n |
+        _     => true
+  end.
+
+Fixpoint all_but_first_le l n :=
+  match l with
+    x::l => all_le l n |
+      _  => true
   end.
 
 Definition max a b :=
@@ -69,41 +61,21 @@ Fixpoint max_lists l1 l2 :=
       _  ,   _   => []
   end.
 
-Fixpoint extract o l1 l2 b :=
-  match l1 with
-    x::[] => if optZ_eq x (Some o) then (l2, b) else (l2, false) |
-    x::l1 => extract o l1 (l2 ++ [x]) b                          |
-     []   => (l2, false)
+Definition extract o s b :=
+  match s with
+    x::s => if optZ_eq x (Some o) then (s, b) else (s, false) |
+     []  => ([], false)
   end.
 
-Lemma fst_extract' : forall o a l1 l2 b,
-  fst (extract o (l1 ++ [a]) l2 b) = l2 ++ l1.
+Lemma extract_true : forall s b,
+  snd (extract 0 s b) = true ->
+  b = true.
 Proof.
-  intros o a l1 l2 b.
-  generalize dependent l2.
-  induction l1.
-  - intro l2.
-    destruct a as [z|]; simpl; try (destruct (z =? o), b; simpl);
-    rewrite app_nil_r; reflexivity.
-  - intro l2.
-    simpl.
-    destruct (l1 ++ [a]) eqn:eq.
-      * destruct l1; discriminate eq.
-      * destruct a0 eqn:eqa0;
-        rewrite IHl1;
-        rewrite <- app_assoc;
-        rewrite <- eqa0;
-        replace ([a0] ++ l1) with (a0 :: l1);
-        reflexivity.
-Qed.
-
-Lemma fst_extract : forall o a l b,
-  fst (extract o (l ++ [a]) [] b) = l.
-Proof.
-  intros o a l b.
-  remember (fst (extract o (l ++ [a]) [] b)) as aux.
-  replace l with ([] ++ l).
-  rewrite Heqaux.
-  apply fst_extract'.
-  reflexivity.
+  intros s b H.
+  induction s.
+  simpl in H. discriminate H.
+  simpl in H.
+  destruct (optZ_eq a (Some 0)); simpl in H.
+  apply H.
+  discriminate H.
 Qed.
--- QueueingSystems.v
+++ QueueingSystems.v
@@ -97,33 +97,6 @@ Proof.
     omega.
 Qed.
 
-Theorem exists_function__upper_bounded :
-  ( exists f, f(0%nat) = n0 /\ forall q, is_tangible q -> 
-     f(q) <= n /\ (forall e, is_valid_event e = true ->
-        is_proper_transition q e -> f(xtransition q [e]) >= f(q) + count_event e ))
-  -> n_upper_bounded.
-Proof.
-  unfold n_upper_bounded.
-  intros [f [H H0]] w H1.
-  assert (H2: n0 + count_buffer w <= f(ixtransition w)). {
-    apply buffer_count_leq_f; try split; try (apply H); try (apply H0); try (apply H1).
-  }
-  assert (f (ixtransition w) <= n). {
-    destruct (is_sink_stateb (ixtransition w)) eqn:H3.
-    - apply is_sink_stateb__is_sink_state in H3.
-      contradiction.
-    - destruct H0 with (q:=ixtransition w). {
-        split.
-          - apply isnt_sink_stateb__isnt_sink_state.
-            apply H3.
-          - exists w.
-            reflexivity.
-      }
-      apply H4.
-  }
-  omega.
-Qed.
-
 Fixpoint oth_trans' q n : list state :=
   match n with
     S n => xtransition q [S (S n)] :: oth_trans' q n |
@@ -135,29 +108,38 @@ Fixpoint verify_upper_bound' (m:Z) (s:list (option Z)) (fuel:nat) (q:state) :=
 match fuel with O => s | S fuel =>
 
     if (length s <=? S q)%nat then (* if q is a sink state *)
-        update_last 0 s
-    else if optZ_eq (nth q s None) None then
-        let  s' := update s q m in
+        update s 0%nat 0
+    else if optZ_eq (nth (S q) s None) None then
+        let  s' := update s (S q) m in
         let s'' := max_lists (verify_upper_bound' (m+1) s' fuel (transition q 0%nat)) (verify_upper_bound' (m-1) s' fuel (transition q 1%nat)) in
         let  f  := verify_upper_bound' m s' fuel in
         foreach (oth_trans q) f max_lists s''
-    else if optZ_ge (nth q s None) (Some m) then (* if s[q] >= m *)
-        update_last 0 s
+    else if optZ_ge (nth (S q) s None) (Some m) then (* if s[q] >= m *)
+        update s 0%nat 0
     else (* if s[q] < m *)
-        update_last 1 s
+        update s 0%nat 1
 
 end.
 
 Definition verify_upper_bound :=
-  let s := verify_upper_bound' n0 (initial_solution states_num ++ [Some 1]) (S states_num) 0%nat in
-  extract 0 s [] (all_but_last_le s n).
+  let s := verify_upper_bound' n0 (Some 1 :: initial_solution states_num) (S states_num) 0%nat in
+  extract 0 s (all_but_first_le s n).
 
 Compute verify_upper_bound.
 
-Lemma initial_solution_none : forall m,
-  nth 0 (initial_solution states_num ++ [Some 1]) m = None.
+Lemma initial_solution_none : forall m p,
+  nth 1 (p::initial_solution states_num) m = None.
 Proof.
-Admitted.
+  intros m p.
+  simpl.
+  induction states_num_minus_1. reflexivity.
+  simpl.
+  rewrite app_nth1.
+  apply IHn1.
+  rewrite app_length.
+  simpl.
+  omega.
+Qed.
 
 Theorem verify_upper_bound_correct :
   snd verify_upper_bound = true <-> n_upper_bounded.
@@ -166,23 +148,34 @@ Admitted.
 
 Definition state_upper_bound (q:state) :=
   match nth q (fst verify_upper_bound) None with
-    Some x => x    |
-     None  => n + 1
+    Some x => x |
+     None  => n
   end.
 
-Require Import Coq.ZArith.Zbool.
+Lemma tangible_state_upper_bound : n_upper_bounded ->
+  forall q, state_upper_bound q <= n.
+Proof.
+  (* intros H q.
+  apply verify_upper_bound_correct in H; unfold verify_upper_bound in H.
+  unfold state_upper_bound, verify_upper_bound.
+  remember (all_but_last_le
+            (verify_upper_bound' n0 (initial_solution states_num ++ [Some 1])
+               (S states_num) 0%nat) n);
+  remember (verify_upper_bound' n0 (initial_solution states_num ++ [Some 1])
+            (S states_num) 0%nat) as s;
+  remember (initial_solution states_num ++ [Some 1]) as s0.
+  destruct s. discriminate H.
+  apply extract_true in H. *)
+Admitted.
 
 Lemma q0_upper_bound : state_upper_bound 0%nat = n0.
 Proof.
 Admitted.
 
-Theorem upper_bounded__exists_function : n_upper_bounded ->
-  ( exists f, f(0%nat) = n0 /\ forall q, is_tangible q -> 
-     f(q) <= n /\ (forall e, is_valid_event e = true ->
-        is_proper_transition q e -> f(xtransition q [e]) >= f(q) + count_event e )).
+Lemma transition_upper_bound : n_upper_bounded ->
+  forall q e, is_tangible q -> is_valid_event e = true -> is_proper_transition q e ->
+  state_upper_bound (xtransition q [e]) >= state_upper_bound q + count_event e.
 Proof.
-  intro H.
-  exists state_upper_bound.
 Admitted.
 
 Theorem iff_exists_function__upper_bounded :
@@ -191,5 +184,33 @@ Theorem iff_exists_function__upper_bounded :
         is_proper_transition q e -> f(xtransition q [e]) >= f(q) + count_event e ))
   <-> n_upper_bounded.
 Proof.
-  split. apply exists_function__upper_bounded. apply upper_bounded__exists_function.
+  split.
+
+  - unfold n_upper_bounded.
+    intros [f [H H0]] w H1.
+    assert (H2: n0 + count_buffer w <= f(ixtransition w)). {
+      apply buffer_count_leq_f; try split; try (apply H); try (apply H0); try (apply H1).
+    }
+    assert (f (ixtransition w) <= n). {
+      destruct (is_sink_stateb (ixtransition w)) eqn:H3.
+      - apply is_sink_stateb__is_sink_state in H3.
+        contradiction.
+      - destruct H0 with (q:=ixtransition w). {
+          split.
+            - apply isnt_sink_stateb__isnt_sink_state.
+              apply H3.
+            - exists w.
+              reflexivity.
+        }
+        apply H4.
+    }
+    omega.
+
+  - intro H.
+    exists state_upper_bound.
+    split.
+    2: intros q H0; split.
+    + apply q0_upper_bound.
+    + apply tangible_state_upper_bound; auto.
+    + intros e H1 H2; apply transition_upper_bound; auto.
 Qed.
